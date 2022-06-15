d77873cd3cf0963da0e5ee1919de37fae4f13463
chrisalmada10 <chrisalmada10@gmail.com>
2022-06-15 19:21:59 -0300

    se creo h1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>Hola Mundo</h1>
         </a>
       </header>
     </div>
